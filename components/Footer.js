b58bf9359271f88e68e3eef5aad2883b07e45105
John Kim <jkim005@yahoo.com>
2023-04-04 18:22:32 -0500

    create footer component

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 bg-gray-100 px-32 py-14 border-t text-gray-600">
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">ABOUT</h5>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p>
+          <p>How Airbnb Works</p>
+          <p>Investors</p>
+          <p>Newsroom</p>
+        </div>
+  
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">COMMUNITY</h5>
+          <p>Accessibility</p>
+          <p>Associates</p>
+          <p>Frontline Stays</p>
+          <p>Gift Cards</p>
+          <p>Invite Friends</p>
+        </div>
+  
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">HOST</h5>
+          <p>Community Center</p>
+          <p>Host your Home</p>
+          <p>Host an Experience</p>
+          <p>Resource Center</p>
+          <p>Responsible Hosting</p>
+        </div>
+  
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">SUPPORT</h5>
+          <p>Cancellation Options</p>
+          <p>Help Center</p>
+          <p>Neighborhood Support</p>
+          <p>Our COVID-19 Response</p>
+          <p>Trust & Safety</p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import LargeCard from "../components/LargeCard";
 import MediumCard from "../components/MediumCard";
 import SmallCard from "../components/SmallCard";
@@ -47,6 +48,8 @@ export default function Home({ exploreData, cardsData }) {
           buttonText="Get Inspired"
         />
       </main>
+
+      <Footer />
     </div>
   );
 }
