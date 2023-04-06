cd01fd7727c26f2888dec70b86e7609e1b811bf1
John Kim <jkim005@yahoo.com>
2023-04-05 21:02:50 -0500

    add clickable map pins with popup descriptions

--- components/Map.js
+++ components/Map.js
@@ -1,30 +1,58 @@
 import { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import getCenter from "geolib/es/getCenter";
 
 function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
   //transform search results obj into an obj with only longitude and latitude
   const coordinates = searchResults.map((result) => ({
     latitude: result.lat,
     longitude: result.long,
   }));
   //coordinates of the center of locations
-  const center = getCenter(coordinates);
+  const center = { latitude: 33.2, longitude: -118.30 };
   const [viewport, setViewport] = useState({
     width: "100%",
     height: "100%",
     latitude: center.latitude,
     longitude: center.longitude,
-    zoom: 11,
+    zoom: 8.5,
   });
 
   return (
     <ReactMapGL
-      mapStyle="mapbox://styles/jkim005/clg4bjecp007a01mng5qdhlt7"
+      mapStyle="mapbox://styles/jkim005/clg4bjecp007a01mng5qdhlt7?optimize=true"
       mapboxApiAccessToken={process.env.mapbox_key}
       {...viewport}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    ></ReactMapGL>
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker longitude={result.long} latitude={result.lat} offsetLeft={-20} offsetTop={-10}>
+            <p
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl animate-bounce"
+              aria-label="round-push-pin"
+              role="img"
+            >
+              📍
+            </p>
+          </Marker>
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
   );
 }
 
