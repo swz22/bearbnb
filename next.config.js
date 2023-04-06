655bd67a231c644847ff8f9b9c019c7a1d9ed708
John Kim <jkim005@yahoo.com>
2023-04-05 19:40:57 -0500

    update gitignore and retrack next.config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+        domains: ['upload.wikimedia.org', 'a0.muscache.com']
+    },
+    env: {
+        mapbox_key:'pk.eyJ1IjoiamtpbTAwNSIsImEiOiJjbGc0YmVqZTcwNXQ4M2NtaGdxeGJqMHVxIn0.TdcllH2w2JWhX9ShNUEJAg'
+    }
+}
